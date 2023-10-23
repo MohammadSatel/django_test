0eed6df51d4e0ab995042d6e7579fd89471f5ace
MohammadSatel <hamuds95@gmail.com>
2023-10-23 22:08:18 +0300

    added serializer

--- base/views.py
+++ base/views.py
@@ -1,5 +1,17 @@
-from django.http import JsonResponse
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from .models import Product
+from .serializer import ProductSerializer
+
+
 
 
 def index(req):
     return JsonResponse('hello', safe=False)
+
+
+
+
+def myProducts(req):
+    all_products = ProductSerializer(Product.objects.all(), many=True).data
+    return JsonResponse(all_products, safe=False)
